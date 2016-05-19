0327423b5581aa15466a43c5389f1a4fc57b15ff
Panagiotis Kon <kls.panagiotis@gmail.com>
2016-05-19 13:26:11 +0300

    v2

--- data_wordcloud.py
+++ data_wordcloud.py
@@ -8,7 +8,7 @@ from os import path
 from wordcloud import WordCloud
 import sys
 
-def main(argv):
+def wordcloud_generator(argv):
 	print("Loading file: %s" % str(argv))
 	dataset=pd.read_csv(argv,sep='\t')
 	print("Loading finished.")
@@ -16,6 +16,7 @@ def main(argv):
 	img_h=540
 	relative_sc=1
 	wordcloud = WordCloud(width=img_w, height=img_h, relative_scaling=relative_sc)
+	print
 	print("Creating categories' list.")
 	list_categories ={}
 	for index, row in dataset.iterrows():
@@ -24,14 +25,16 @@ def main(argv):
 		except KeyError:
 		   	list_categories[row['Category']] = row['Content']
 	print("Categories created.")
+	print
 	print("Creating word clouds.")
 	for category, words in list_categories.iteritems():
 		wordcloud.generate(words)
 		image = wordcloud.to_image()
 		image.save("data/Wordcloud_" + category + "_" + str(img_w) + "x" + str(img_h) + ".png")
 		image.show()
+	print("Word Clouds finished.")
 	return
 
 print"Program starts..."
-main(sys.argv[1])
-print"Finished."
+wordcloud_generator(sys.argv[1])
+print"Program ends..."
